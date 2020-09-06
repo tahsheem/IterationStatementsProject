7a57ca025d871cacae6e9f219adcebb47b73d1ec
mo ali <tahsheem24@gmail.com>
2020-09-06 09:28:47 -0700

    Wrote the needed lines of code

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -11,19 +11,35 @@ namespace IterationStatements
 
             //Create a List called "numbers" - DONE
             var numbers = new List<int>();
-            
+
             //Create a variable of type int and initialize the variable with a value of 0
 
-
+            var myNumber = 0;
 
             // Create a do-while loop
+            
             {
                 // Increment your variable by 1
                 // Then add your variable to "numbers"
             }
+
+            do
+            {
+                myNumber++;
+
+                numbers.Add(myNumber);
+
+
+            } while (myNumber < 100);
+
+
             // While your variable is less than 100
 
-
+            do
+            {
+                myNumber++;
+                numbers.Add(myNumber);
+            } while (myNumber < 200); ;
 
             // Create a while loop
             // While your variable is less than 200
@@ -37,10 +53,22 @@ namespace IterationStatements
             // Create a foreach loop
             // Write your variable to the console
 
+            foreach (var item in numbers)
+            {
+
+                Console.WriteLine($"{item}");
+            }
+
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
 
             // Create a for loop
+
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
+            {
+                Console.WriteLine($"{numbers[i]}");
+            }
+
             // in your initializer set the value of i to 199
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
